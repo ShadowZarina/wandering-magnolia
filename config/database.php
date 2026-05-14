84bd53369287451d01fe11472e004b3161902d3e
Alfer Mercado <alfzmercado240@gmail.com>
2026-05-14 19:53:06 +0800

    Fix gitignore

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'wandering_magnolias');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_CHARSET', 'utf8mb4');
-
-
--- config/mail.php
+++
@@ -1,7 +0,0 @@
-    
-<?php 
-    define('MAIL_HOST','smtp.gmail.com');
-    define('MAIL_PORT', 587);
-    define('MAIL_USER', 'alfzmercado240@gmail.com');     
-    define('MAIL_PASS', 'odyu ilwr rmjg cona'); 
-    define('MAIL_FROM_NAME', 'Wandering Magnolia');
